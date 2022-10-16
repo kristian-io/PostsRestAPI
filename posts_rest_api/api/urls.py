64dabf6a23f1b9976b6c2b3fcf481c0737523ec7
Kristian <>
2022-10-16 16:32:54 +0200

    add openapi schema view

--- posts_rest_api/api/urls.py
+++ posts_rest_api/api/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.schemas import get_schema_view
 
 from api import views
 
 urlpatterns = [
     path('posts', views.PostList.as_view()),
-    path('posts/', views.PostList.as_view()),
+    # path('posts/', views.PostList.as_view()),
     path("posts/<int:pk>", views.PostDetail.as_view()),
     path("posts/user/<int:userId>", views.PostUserDetail.as_view()),
+    path('openapi', get_schema_view(
+        title="Post REST API",
+        description="REST API for managing user posts.",
+        version="1.0.0"
+    ), name='openapi-schema'),    
 ]
 
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+# causes issues when automatically generating openapi schema...
+# urlpatterns = format_suffix_patterns(urlpatterns)
