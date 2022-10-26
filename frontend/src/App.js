89d4648e741eac77caf54f165e7f5605645e2534
Kristian <>
2022-10-26 07:42:57 +0200

    add button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+
 import { Container, TextField } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
+
+import './App.css';
 
 const darkTheme = createTheme({
   palette: {
@@ -13,25 +16,25 @@ const darkTheme = createTheme({
 });
 
 
-function UserInput({ value=null, label = null }) {
+function UserInput({ value = null, label = null }) {
   return (
-    <TextField 
-      id="outlined-basic" 
-      label={label} 
+    <TextField
+      id="outlined-basic"
+      label={label}
       variant="outlined"
       value={value}
-      // helperText="User ID"
+    // helperText="User ID"
     />
   )
 }
 
-function TitleInput({ value=null, label = null }) {
+function TitleInput({ value = null, label = null }) {
   return (
     <TextField
-    id="outlined-multiline-static"
-    label={label}
-    value={value}
-    fullWidth
+      id="outlined-multiline-static"
+      label={label}
+      value={value}
+      fullWidth
     // helperText="Title of your post"
     // defaultValue="Title of your post."
     />
@@ -42,51 +45,66 @@ function TitleInput({ value=null, label = null }) {
 function ContentInput({ label = null }) {
   return (
     <TextField
-    id="outlined-multiline-static margin-normal"
-    label={label}
-    multiline
-    rows={4}
-    fullWidth 
-    // defaultValue="Write your post text here."
-    helperText="Write your post :) "
+      id="outlined-multiline-static margin-normal"
+      label={label}
+      multiline
+      rows={4}
+      fullWidth
+      // defaultValue="Write your post text here."
+      helperText="Write your post :) "
     />
   )
 }
 
 
+function SaveButton() {
+
+  return (
+    <Button 
+      variant="contained"
+      sx={{
+        justifySelf: 'center'
+      }}  
+    >Save
+    </Button>
+  )
+}
+
 
 function PostContainer() {
   return (
     <div className="post_container">
-      <Container 
-        sx = {{
+      <Container
+        sx={{
           maxWidth: '70%'
         }}>
-      <h1> Create a new post</h1>
-      <Box
-      component="form"
-      noValidate
-      sx={{
-        display: 'grid',
-        gap: 10,
-      }}
-      >
-        <UserInput
-          label="User ID"
+        <h1> Create a new post</h1>
+        <Box
+          component="form"
+          noValidate
+          sx={{
+            display: 'grid',
+            gap: 5,
+          }}
         >
-        </UserInput>
-        {/* <hr/> */}
-        <TitleInput
-          label="Title"
-        >
-        </TitleInput>
-        {/* <hr/> */}
-        <ContentInput
-          label="Content">
-        </ContentInput>
-      </Box>
+          <UserInput
+            label="User ID"
+          >
+          </UserInput>
+          {/* <hr/> */}
+          <TitleInput
+            label="Title"
+          >
+          </TitleInput>
+          {/* <hr/> */}
+          <ContentInput
+            label="Content">
+          </ContentInput>
+
+        <SaveButton />
+        </Box>
       </Container>
-        </div>
+    </div>
   )
 }
 
